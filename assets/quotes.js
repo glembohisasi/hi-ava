ef1cc54560d22f48e045e5180324ad5517a77872
glembohisasi <64810948+glembohisasi@users.noreply.github.com>
2021-04-29 13:52:23 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,9 @@
 // prettier-ignore
 export default [
-    { author: "Glembohisasi", quotes: "Kurasa aku telah menyukaimu. Sejak pertama kali bertemu" },
-    { author: "Glembohisasi", quotes: "Hay, terimakasih telah Hadir di hidupku" },
-    { author: "Glembohisasi", quotes: "Kuharap kau tidak menutup ini, karna aku ingin mengatakan" },
-    { author: "Glembohisasi", quotes: "I love you..." },
-    { author: "Glembohisasi", quotes: "Tunggulah kupinang kau dengan kata Bismillah" },
-    { author: "Glembohisasi", quotes: "Simpanlah ini jika suatu saat kau rindu padaku" },
-    { author: "Glembohisasi", quotes: "I'm sorry for loving you." },
+    { author: "Glembohisasi", quotes: "Haii, ga kerasa ya, kita udah menghadapi ini 2 tahun" },
+    { author: "Glembohisasi", quotes: "Gimana? Masih semangat kan?" },
+    { author: "Glembohisasi", quotes: "Yuk bisa yuk, aku tau kita pasti bisa" },
+    { author: "Glembohisasi", quotes: "Lewati semua ini" },
+    { author: "Glembohisasi", quotes: "Tetap semangat ya" },
+    { author: "Glembohisasi", quotes: "Kita pasti bisa kok lewati semua ini" },
 ];
